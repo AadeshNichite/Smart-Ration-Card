eb2c7cb1381033677a468bcd2320057b4252725a
AadeshNichite <54974312+AadeshNichite@users.noreply.github.com>
2019-11-25 12:02:41 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import LoginPage from './components/LoginPage/LoginPage'
+import LoginPage from './components/LoginPage/LoginPage';
+import LoginPageAdmin from './components/LoginPageAdmin/LoginPageAdmin';
 
 function App() {
   return (
     <div className="App">
       <LoginPage />
+      <LoginPageAdmin />
     </div>
   );
 }
