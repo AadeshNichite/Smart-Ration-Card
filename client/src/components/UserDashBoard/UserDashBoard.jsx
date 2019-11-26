bbd12c9b552c5ab195b7e2cc5d6158240610c70a
AadeshNichite <54974312+AadeshNichite@users.noreply.github.com>
2019-11-26 14:51:41 +0530

    added logout component

--- client/src/components/UserDashBoard/UserDashBoard.jsx
+++ client/src/components/UserDashBoard/UserDashBoard.jsx
@@ -1,8 +1,26 @@
 import React, {Component} from 'react';
 import './UserDashBoard.css';
+import { Redirect } from 'react-router-dom';
+// import  Logout  from '../Logout/Logout';
 
 class UserDashBoard extends Component{
+    constructor(props){
+        super(props)
+        const token = localStorage.getItem("token");
+
+        let loggedIn = true;
+        if(token == null){
+            loggedIn = false;
+        }
+        this.state ={
+            loggedIn
+        }
+    }
     render(){
+        if(this.state.loggedIn === false)
+        {
+            return <Redirect to="/" />
+        }
         return <div className="MainDiv">
         <h1 className="head text-center">Smart-Ration Card</h1>
         </div>
