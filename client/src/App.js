cc996b4bdeda9d5ea9a193df4fb1da1902f3967b
AadeshNichite <54974312+AadeshNichite@users.noreply.github.com>
2019-11-26 13:20:14 +0530

    Update

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import './App.css';
 import LoginPage from './components/LoginPage/LoginPage';
 import { link, Switch , Route } from 'react-router-dom';
-import { RegisterPage } from './components/RegisterPage/RegisterPage'
+import { RegisterPage } from './components/RegisterPage/RegisterPage';
+import  UserDashBoard from './components/UserDashBoard/UserDashBoard';
 
 function App() {
   return (
     <div className="App">
      <Switch>
         <Route exact path="/" component = {LoginPage} />
-        <Route exact path="/register" component={RegisterPage} />
+        <Route path="/register" component={RegisterPage} />
+        <Route path="/UserDashBoard" component={UserDashBoard} />
       </Switch>
       
     </div>
