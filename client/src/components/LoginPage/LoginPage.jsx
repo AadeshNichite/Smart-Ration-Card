125a08be60113e291ce269d5d302e2cfe6ab37a0
AadeshNichite <54974312+AadeshNichite@users.noreply.github.com>
2019-11-26 12:37:51 +0530

    LoginPage with auth

--- client/src/components/LoginPage/LoginPage.jsx
+++ client/src/components/LoginPage/LoginPage.jsx
@@ -3,23 +3,41 @@ import './LoginPage.css';
 import { Link, Switch , Route } from 'react-router-dom';
 
 class LoginPage extends Component{
-    // constructor(props)
-    // {
-    //     let loggedIn = false;
-    //     this.state ={
-    //         username: '',
-    //         password: ''
-    //     }
-    // }
+    constructor(props)
+    {
+        super(props);
+        let loggedIn = false;
+        this.state ={
+            username: '',
+            password: ''
+        }
+        this.onChange=this.onChange.bind(this);
+        this.submitForm=this.submitForm.bind(this);
+    }
+    onChange(e){
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    submitForm(e){
+        e.preventDefault()
+        const{username,password} = this.state
+
+        //Login Logic
+
+    }
 
     render(){
         return (
         <div className="MainDiv">
-            <h1 className="head text-center">Smart-Ration Card</h1>
-            <input type="text" className="data" id="data" placeholder="Enter Ration Card Number" />
-            <input type="password" className="data" id="data" placeholder="Enter Password" />
-            <button type="submit" className="button">submit</button>
-            <Link to='/register'><button type="submit" className="button">Register</button></Link>
+            <form onSubmit={this.submitForm}>
+                <h1 className="head text-center">Smart-Ration Card</h1>
+                <input type="text" className="data" id="data" name="username" placeholder="Enter Ration Card Number" value={this.state.username} onChange={this.onChange} />
+                <input type="password" className="data" id="data" name="password" placeholder="Enter Password" value={this.state.password} onChange={this.onChange} />
+                <button type="submit" className="button">submit</button>
+                <Link to='/register'><button type="submit" className="button">Register</button></Link>
+            </form>
         </div>
      )
     }
