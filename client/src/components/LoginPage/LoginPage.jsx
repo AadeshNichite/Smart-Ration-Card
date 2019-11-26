2a161874be84024fe7904ea932d38f3caaa42d24
AadeshNichite <54974312+AadeshNichite@users.noreply.github.com>
2019-11-26 12:11:43 +0530

    LoginPage and Registration page componanter created

--- client/src/components/LoginPage/LoginPage.jsx
+++ client/src/components/LoginPage/LoginPage.jsx
@@ -1,16 +1,27 @@
 import React, {Component} from 'react';
 import './LoginPage.css';
+import { Link, Switch , Route } from 'react-router-dom';
 
 class LoginPage extends Component{
+    // constructor(props)
+    // {
+    //     let loggedIn = false;
+    //     this.state ={
+    //         username: '',
+    //         password: ''
+    //     }
+    // }
 
     render(){
-        return <div className="MainDiv">
-        <h1 className="head text-center">Smart-Ration Card</h1>
-        <input type="text" className="data" id="data" placeholder="Enter Ration Card Number" />
-        {/* <button type="submit" className="button" onClick={()=> this.show()}>submit</button> */}
-        <button type="submit" className="button">submit</button>
+        return (
+        <div className="MainDiv">
+            <h1 className="head text-center">Smart-Ration Card</h1>
+            <input type="text" className="data" id="data" placeholder="Enter Ration Card Number" />
+            <input type="password" className="data" id="data" placeholder="Enter Password" />
+            <button type="submit" className="button">submit</button>
+            <Link to='/register'><button type="submit" className="button">Register</button></Link>
         </div>
+     )
     }
 }
-
 export default LoginPage;
