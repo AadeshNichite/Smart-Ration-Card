f971bac8700c42630276501a75fb71686af5200c
AadeshNichite <54974312+AadeshNichite@users.noreply.github.com>
2019-11-27 12:11:00 +0530

    Schema added

---
+++ server/models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    rationCardNo: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required:true,
+        unique: true
+    },
+    noOfPeople: {
+        type:Number,
+        required:true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required:true
+    },
+    date :{
+        type:Date,
+        default:Date.now
+    }  
+})
+module.exports = User = mongoose.model('user',UserSchema);
