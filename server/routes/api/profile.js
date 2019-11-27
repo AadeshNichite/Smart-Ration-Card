d5dac6454e2e03d02fe4c5dc6cf6e3f158917dd1
AadeshNichite <54974312+AadeshNichite@users.noreply.github.com>
2019-11-27 16:54:54 +0530

    Update

--- server/routes/api/profile.js
+++ server/routes/api/profile.js
@@ -1,10 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/profile');
+const User = require('../../models/user');
+const {check ,validationResult } = require('express-validator/check');
 
-//@route   GET api/profile
-//@desc     Add profile
-//@access   Public
+//@route    GET api/profile
+//@desc     Get Current User profile
+//@access   Private
 
-router.get('/',(req,res) => res.send('User Route'));
+router.get('/', auth , async (req,res) => {
+    try{
+        const profile = await Profile.findOne({user : req.user.id}).populate('user',
+        ['name','rationCardNo','address','noOfPeople']);
+
+        if(!profile){
+            return res.status(400).json({msg:"No profile avilable.."});
+        }
+
+        res.json(profile);
+    }catch(err){
+        console.log(err.message);
+        res.status(500).send("server error");
+    }
+
+
+});
+
+//@route    POST api/profile
+//@desc     Create and Update User profile
+//@access   Private
+
+
 
 module.exports = router;
