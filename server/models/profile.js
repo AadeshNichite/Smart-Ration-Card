a1a78768cdc8efb0504257fef91c0b31bb8d5e3b
AadeshNichite <54974312+AadeshNichite@users.noreply.github.com>
2019-11-27 16:17:43 +0530

    Added  profile schema

---
+++ server/models/profile.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const ProfileSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    rationhistory: {
+        Month: {
+            type : Date,
+            required : true
+        },
+        history:{
+            item: {
+                type : String,
+                required : true
+            },
+            ammount: {
+                type : String,
+                required : true
+            },
+            price :{
+                type : String,
+                required : true
+            }
+        }
+
+    }    
+
+})
