2aeac2f4a10af0bd14463ecc06ca499b9572076e
AadeshNichite <54974312+AadeshNichite@users.noreply.github.com>
2019-11-28 16:19:19 +0530

    Update model

--- server/models/profile.js
+++ server/models/profile.js
@@ -5,7 +5,7 @@ const ProfileSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'user'
     },
-    rationhistory: {
+    rationhistory: [{
         Month: {
             type : String,
             required : true
@@ -25,7 +25,7 @@ const ProfileSchema = new mongoose.Schema({
             }
         }
 
-    }    
+    }]    
 
 })
 
