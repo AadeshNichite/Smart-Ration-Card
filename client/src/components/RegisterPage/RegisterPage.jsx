9e3919c71cdf487e526f1c995e9409e7fc8470c5
AadeshNichite <54974312+AadeshNichite@users.noreply.github.com>
2019-11-30 14:26:26 +0530

    signUp done

--- client/src/components/RegisterPage/RegisterPage.jsx
+++ client/src/components/RegisterPage/RegisterPage.jsx
@@ -9,27 +9,28 @@ class RegisterPage extends Component {
         super(props);
         let loggedIn = false;
         this.state ={
-            rationNumber: '',
-            name : '',
-            noOfPeople: '',
-            address:'',
-            reEnteredPassword:'',
-            password: ''
+                rationCardNo: '',
+                name : '',
+                noOfPeople: '',
+                address:'',
+                reEnteredPassword:'',
+                password: ''
+               
         }
-        this.onChange=this.onChange.bind(this);
+        this.handleChange=this.handleChange.bind(this);
         this.submitForm=this.submitForm.bind(this);
     }
-    onChange(e){
+    handleChange(e){
         const {name,value} = e.target
         this.setState({
             [name]: value
-
         })
     }
 
       submitForm(e){
         e.preventDefault();
-        const rationNumber = this.state.rationNumber;
+        console.log(this.state.rationCardNo)
+        const rationCardNo = this.state.rationCardNo;
         const name = this.state.name;
         const noOfPeople = this.state.noOfPeople;
         const address = this.state.address;
@@ -40,7 +41,7 @@ class RegisterPage extends Component {
                 loggedIn : false
             });
         }
-        const user = {rationNumber,name,noOfPeople,address,password}
+        const user = {rationCardNo,name,noOfPeople,address,password}
 
         const config = {
             headers : {
@@ -51,7 +52,8 @@ class RegisterPage extends Component {
          const body =  JSON.stringify(user);
         axios.post('/api/users',body,config)
                          .then(res => {
-                             localStorage.setItem("token",res.data.token);
+                            //  localStorage.setItem("token",res.data.token);
+                             sessionStorage.setItem("token",res.data.token);
                              if(res.data.token){
                                 this.setState({
                                    loggedIn : true
@@ -67,15 +69,15 @@ class RegisterPage extends Component {
         }   
         return (
         <div>
-            <form onSubmit={this.submitForm} className="MainDiv">
+            <form className="MainDiv">
                 <h1 className="headRegister text-center">Smart-Ration Card</h1>
-                <input type="text" className="dataRegister" id="rationCardNo" name="rationCardNo" placeholder="Enter Ration Card Number" value={this.state.rationCardNo} onChange={this.onChange} />
-                <input type="text" className="dataRegister" id="name" name="name" placeholder="Enter Name" value={this.state.name} onChange={this.onChange} />
-                <input type="text" className="dataRegister" id="noOfPeople" name="noOfPeople" placeholder="Enter Number of People" value={this.state.noOfPeople} onChange={this.onChange} />
-                <input type="text" className="dataRegister" id="address" name="address" placeholder="Address" value={this.state.address} onChange={this.onChange} />
-                <input type="password" className="dataRegister" id="password" name="password" placeholder="Password" value={this.state.password} onChange={this.onChange} />
-                <input type="password" className="dataRegister" id="reEnteredPassword" name="reEnteredPassword" placeholder="Re Enter Password" value={this.state.reEnteredPassword} onChange={this.onChange} />
-                <button type="submit" className="buttonRegister">submit</button> 
+                <input type="text" className="dataRegister" id="rationCardNo" name="rationCardNo" placeholder="Enter Ration Card Number" onChange={this.handleChange} />
+                <input type="text" className="dataRegister" id="name" name="name" placeholder="Enter Name" onChange={this.handleChange} />
+                <input type="text" className="dataRegister" id="noOfPeople" name="noOfPeople" placeholder="Enter Number of People" onChange={this.handleChange} />
+                <input type="text" className="dataRegister" id="address" name="address" placeholder="Address" onChange={this.handleChange} />
+                <input type="password" className="dataRegister" id="password" name="password" placeholder="Password"  onChange={this.handleChange} />
+                <input type="password" className="dataRegister" id="reEnteredPassword" name="reEnteredPassword" placeholder="Re Enter Password" onChange={this.handleChange} />
+                <button type="submit" className="buttonRegister" onClick={this.submitForm}>submit</button> 
             </form>
         </div>
         )
